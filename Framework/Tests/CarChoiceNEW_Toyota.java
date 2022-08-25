5704393fb88ee3713fd142e5fc4d5d38f7957ef4
paul.wright@curiosity.software <paul.wright@curiosity.software>
2022-08-25 15:07:05 +0000

    Committing tests for model 'CarChoice NEW'

---
+++ Framework/Tests/CarChoiceNEW_Toyota.java
@@ -0,0 +1,170 @@
+package tests;
+
+import reports.TestNGListener;
+import ie.curiositysoftware.testmodeller.TestModellerPath;
+import ie.curiositysoftware.testmodeller.TestModellerSuite;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import ie.curiositysoftware.allocation.dto.ResultMergeMethod;
+import ie.curiositysoftware.allocation.dto.DataAllocationRow;
+import reports.ExtentTestListener;
+import ie.curiositysoftware.allocation.dto.DataAllocationResult;
+import ie.curiositysoftware.allocation.engine.DataAllocation;
+import utilities.testmodeller.TestModellerLogger;
+
+//https://internalcsitraining.cloud.testinsights.io/app/#!/model-engine/guid/d0ccadc8-da2c-4956-83fa-e527653e0609
+@Listeners(TestNGListener.class)
+@TestModellerSuite(id = 621, profileId = 100831)
+public class CarChoiceNEW_Toyota extends TestBase
+{
+    
+
+    
+    @Test  (groups= {"CarChoice NEW","CarChoice NEW - Toyota"})
+    @TestModellerPath(guid = "a97997c8-7d0b-4675-b29b-ab47cdb36148")
+    public void PositiveSelectProductionyearSelectMakeToyotaSelectTypeToyotaSelectModelToyota()
+    {
+        
+        pages.HyperonChoice _HyperonChoice = new pages.HyperonChoice(driver);
+    TestModellerLogger.SetLastNodeGuid("008160bc-8404-4aaa-90e1-37995b93d0da");
+    _HyperonChoice.Select_Production_year("");
+
+    TestModellerLogger.SetLastNodeGuid("d4a10579-3cf6-4278-8a27-e0c5f646c913");
+    _HyperonChoice.Select_Make("Toyota");
+
+    TestModellerLogger.SetLastNodeGuid("1160918b-d5c1-440f-be5a-00003fd3de29");
+    _HyperonChoice.Select_Type("");
+
+    TestModellerLogger.SetLastNodeGuid("f1c0d0d4-b9ab-4ddc-b7f7-82ec9262e3fe");
+    _HyperonChoice.Select_Model("");
+
+    }
+
+
+    @Test  (groups= {"CarChoice NEW","CarChoice NEW - Toyota"})
+    @TestModellerPath(guid = "7e74bc93-ffb3-4b6d-aa07-dbf766075289")
+    public void PositiveSelectProductionyearSelectMakeToyotaSelectTypeToyotaSelectModelToyota1()
+    {
+        
+        pages.HyperonChoice _HyperonChoice = new pages.HyperonChoice(driver);
+    TestModellerLogger.SetLastNodeGuid("008160bc-8404-4aaa-90e1-37995b93d0da");
+    _HyperonChoice.Select_Production_year("");
+
+    TestModellerLogger.SetLastNodeGuid("d4a10579-3cf6-4278-8a27-e0c5f646c913");
+    _HyperonChoice.Select_Make("Toyota");
+
+    TestModellerLogger.SetLastNodeGuid("1160918b-d5c1-440f-be5a-00003fd3de29");
+    _HyperonChoice.Select_Type("");
+
+    TestModellerLogger.SetLastNodeGuid("f1c0d0d4-b9ab-4ddc-b7f7-82ec9262e3fe");
+    _HyperonChoice.Select_Model("");
+
+    }
+
+
+    @Test  (groups= {"CarChoice NEW","CarChoice NEW - Toyota"})
+    @TestModellerPath(guid = "0152b6b2-cb52-46d6-8e4f-cd12ac7c34af")
+    public void PositiveSelectProductionyearSelectMakeToyotaSelectTypeToyotaSelectModelToyota2()
+    {
+        
+        pages.HyperonChoice _HyperonChoice = new pages.HyperonChoice(driver);
+    TestModellerLogger.SetLastNodeGuid("008160bc-8404-4aaa-90e1-37995b93d0da");
+    _HyperonChoice.Select_Production_year("False");
+
+    TestModellerLogger.SetLastNodeGuid("d4a10579-3cf6-4278-8a27-e0c5f646c913");
+    _HyperonChoice.Select_Make("Toyota");
+
+    TestModellerLogger.SetLastNodeGuid("1160918b-d5c1-440f-be5a-00003fd3de29");
+    _HyperonChoice.Select_Type("");
+
+    TestModellerLogger.SetLastNodeGuid("f1c0d0d4-b9ab-4ddc-b7f7-82ec9262e3fe");
+    _HyperonChoice.Select_Model("");
+
+    }
+
+
+    @Test  (groups= {"CarChoice NEW","CarChoice NEW - Toyota"})
+    @TestModellerPath(guid = "48afd927-dd57-4ed3-9b99-a2e7962c07f4")
+    public void PositiveSelectProductionyearSelectMakeToyotaSelectTypeToyotaSelectModelToyota3()
+    {
+        
+        pages.HyperonChoice _HyperonChoice = new pages.HyperonChoice(driver);
+    TestModellerLogger.SetLastNodeGuid("008160bc-8404-4aaa-90e1-37995b93d0da");
+    _HyperonChoice.Select_Production_year("");
+
+    TestModellerLogger.SetLastNodeGuid("d4a10579-3cf6-4278-8a27-e0c5f646c913");
+    _HyperonChoice.Select_Make("Toyota");
+
+    TestModellerLogger.SetLastNodeGuid("1160918b-d5c1-440f-be5a-00003fd3de29");
+    _HyperonChoice.Select_Type("");
+
+    TestModellerLogger.SetLastNodeGuid("f1c0d0d4-b9ab-4ddc-b7f7-82ec9262e3fe");
+    _HyperonChoice.Select_Model("");
+
+    }
+
+
+    @Test  (groups= {"CarChoice NEW","CarChoice NEW - Toyota"})
+    @TestModellerPath(guid = "260b9cd3-427f-441a-9c0d-e3bc25d14bf8")
+    public void PositiveSelectProductionyearSelectMakeToyotaSelectTypeToyotaSelectModelToyota4()
+    {
+        
+        pages.HyperonChoice _HyperonChoice = new pages.HyperonChoice(driver);
+    TestModellerLogger.SetLastNodeGuid("008160bc-8404-4aaa-90e1-37995b93d0da");
+    _HyperonChoice.Select_Production_year("");
+
+    TestModellerLogger.SetLastNodeGuid("d4a10579-3cf6-4278-8a27-e0c5f646c913");
+    _HyperonChoice.Select_Make("Toyota");
+
+    TestModellerLogger.SetLastNodeGuid("1160918b-d5c1-440f-be5a-00003fd3de29");
+    _HyperonChoice.Select_Type("");
+
+    TestModellerLogger.SetLastNodeGuid("f1c0d0d4-b9ab-4ddc-b7f7-82ec9262e3fe");
+    _HyperonChoice.Select_Model("");
+
+    }
+
+
+    @Test  (groups= {"CarChoice NEW","CarChoice NEW - Toyota"})
+    @TestModellerPath(guid = "c3284348-762d-44bb-8989-3ec1a04359b8")
+    public void PositiveSelectProductionyearSelectMakeToyotaSelectTypeToyotaSelectModelToyota5()
+    {
+        
+        pages.HyperonChoice _HyperonChoice = new pages.HyperonChoice(driver);
+    TestModellerLogger.SetLastNodeGuid("008160bc-8404-4aaa-90e1-37995b93d0da");
+    _HyperonChoice.Select_Production_year("");
+
+    TestModellerLogger.SetLastNodeGuid("d4a10579-3cf6-4278-8a27-e0c5f646c913");
+    _HyperonChoice.Select_Make("Toyota");
+
+    TestModellerLogger.SetLastNodeGuid("1160918b-d5c1-440f-be5a-00003fd3de29");
+    _HyperonChoice.Select_Type("");
+
+    TestModellerLogger.SetLastNodeGuid("f1c0d0d4-b9ab-4ddc-b7f7-82ec9262e3fe");
+    _HyperonChoice.Select_Model("");
+
+    }
+
+
+    @Test  (groups= {"CarChoice NEW","CarChoice NEW - Toyota"})
+    @TestModellerPath(guid = "1a3a895f-a547-4b9a-b42d-b9457e33bb72")
+    public void PositiveSelectProductionyearSelectMakeToyotaSelectTypeToyotaSelectModelToyota6()
+    {
+        
+        pages.HyperonChoice _HyperonChoice = new pages.HyperonChoice(driver);
+    TestModellerLogger.SetLastNodeGuid("008160bc-8404-4aaa-90e1-37995b93d0da");
+    _HyperonChoice.Select_Production_year("");
+
+    TestModellerLogger.SetLastNodeGuid("d4a10579-3cf6-4278-8a27-e0c5f646c913");
+    _HyperonChoice.Select_Make("Toyota");
+
+    TestModellerLogger.SetLastNodeGuid("1160918b-d5c1-440f-be5a-00003fd3de29");
+    _HyperonChoice.Select_Type("");
+
+    TestModellerLogger.SetLastNodeGuid("f1c0d0d4-b9ab-4ddc-b7f7-82ec9262e3fe");
+    _HyperonChoice.Select_Model("");
+
+    }
+
+
+}
